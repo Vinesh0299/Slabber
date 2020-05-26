e43279b26a041181882a579bf3455a40c614d714
Vinesh <Vinesh.katewa@gmail.com>
2020-05-26 19:45:54 +0530

    Added database connection file to gitignore

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
-// Importing database connection instance
-const getDbIns = require('./models/dbconnection.js');
-
-getDbIns.then((db) => {
-    const inventory = db.collection("inventory");
-    inventory.insertOne({name: "Madara Uchiha", age: 180})
-    .then((result) => {
-        inventory.find({}).toArray((err, items) => {
-            console.log(items);
-        });
-    });
-});
 
 // Importing created routes
 const chatRoutes = require('./routes/chats.js');
@@ -24,10 +10,10 @@ const userRoutes = require('./routes/user.js');
 // Importing created socket events
 require('./socket/groupchat.js')(io);
 
-const PORT = 3000 || process.env.PORT;
-
 // Mounting routes on the app
 app.use('/', chatRoutes);
 app.use('/', userRoutes);
+
+const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
