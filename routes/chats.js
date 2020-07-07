aa7834bcf4e10feb026386badf07716dba814b54
Vinesh <Vinesh.katewa@gmail.com>
2020-07-07 16:29:00 +0530

    Completed modifying the chats api routes

--- routes/chats.js
+++ routes/chats.js
@@ -8,10 +8,6 @@ const ObjectId = require('mongodb').ObjectId;
 app.set('view engine', 'ejs');
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.send('FUCK YOU');
-});
-
 // Route to handle chatrooms
 app.get('/chatroom/:roomid', (req, res, next) => {
     var roomid = req.params.roomid;
@@ -30,8 +26,8 @@ app.post('/createroom', (req, res, next) => {
     dbIns.then((db) => {
         const Chatrooms = db.collection('Chatrooms');
         const Users = db.collection('Users');
-        // Check if username actually exists
-        Users.find({username: data.username}).toArray((err, items) => {
+        // Check if email actually exists
+        Users.find({email: data.email}).toArray().then((items) => {
             const chatroom = new groups({
                 name: data.name
             });
@@ -42,36 +38,42 @@ app.post('/createroom', (req, res, next) => {
                 Chatrooms.updateOne({_id: chatroom._id}, { $set: { admin: {
                     "$ref": 'User',
                     "$id": new ObjectId(adminId),
-                    "$db": 'test'
+                    "$db": 'Slabber'
                 } } });
                 data.members.map((member) => {
-                    Users.find({username: member}).toArray((err, items) => {
+                    Users.find({email: member}).toArray().then((items) => {
                         if(items.length > 0) {
                             // Update the memberList array for the chatroom
                             Chatrooms.updateOne({_id: chatroom._id}, { $push: { memberList: {
                                 memberId: {
                                     "$ref": 'User',
                                     "$id": new ObjectId(items[0]._id),
-                                    "$db": 'test'
+                                    "$db": 'Slabber'
                                 },
                                 memberName: items[0].fullname
                             } } });
                             // Add this chatroom to each member's chatroom list
-                            Users.updateOne({username: items[0].username}, { $push: { chatRoomList: {
+                            Users.updateOne({email: items[0].email}, { $push: { chatRoomList: {
                                 roomId: {
                                     "$ref": 'Chatroom',
                                     "$id": new ObjectId(chatroom._id),
-                                    "$db": 'test'
+                                    "$db": 'Slabber'
                                 },
                                 roomName: data.name
-                            } } }).then((items) => {
-                                res.send({"error": 0, "message": "Chatroom created successfully!"});
-                            });
+                            } } });
                         }
+                    }).then((items) => {
+                        res.status(200).json({message: 'Chatroom created successfully'});
                     });
                 });
+            }).catch((err) => {
+                console.log(err);
+                res.status(500).json({message: 'Error occured while creating the chatroom'});
             });
         });
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json({message: 'Error occured while connecting to the database'});
     });
 });
 
@@ -82,35 +84,51 @@ app.post('/createPrivateChat', (req, res, next) => {
         const privateChats = db.collection('PrivateChats');
         const Users = db.collection('Users');
         // Getting ids of both users and checking if both of them exists
-        Users.find({ $or: [ {username: data.username}, {username: data.friendUsername} ] }).toArray((err, items) => {
-            if(items.length > 1) {    
+        Users.find({ $or: [ {email: data.email}, {email: data.friendemail} ] }).toArray().then((items) => {
+            if(items.length > 1) {
+                const user = items[0].fullname;
+                const friend = items[1].fullname;
                 const privatechat = new privateChat();
                 privateChats.insertOne(privatechat);
                 privateChats.updateOne({_id: privatechat._id}, { $set: {
                     person1: {
                         "$ref": 'User',
                         "$id": items[0]._id,
-                        "$db": 'test'
+                        "$db": 'Slabber'
                     },
                     person2: {
                         "$ref": 'User',
                         "$id": items[1]._id,
-                        "$db": 'test'
+                        "$db": 'Slabber'
                     }
                 } }).then((items) => {
                     // Updating User information by adding the private chat data to their database information
-                    Users.updateMany({ $or: [ {username: data.username}, {username: data.friendUsername} ] }, { $push: { privateChats: {
-                        "$ref": 'PrivateChat',
-                        "$id": privatechat._id,
-                        "$db": 'test'
+                    Users.update({email: data.email}, { $push: { privateChats: {
+                        chatId: {
+                            "$ref": 'PrivateChat',
+                            "$id": privatechat._id,
+                            "$db": 'Slabber'
+                        },
+                        friendName: friend
+                    } } });
+                    Users.update({email: data.friendemail}, { $push: { privateChats: {
+                        chatId: {
+                            "$ref": 'PrivateChat',
+                            "$id": privatechat._id,
+                            "$db": 'Slabber'
+                        },
+                        friendName: user
                     } } });
                 }).then((items) => {
                     // sending success message
-                    res.send({"error": 0, "message": "Private chatroom created, you can now start chatting"});
+                    res.status(200).json({message: "Private chatroom created, you can now start chatting"});
                 });
             // Sending error message if the users does not exist
-            } else res.send({"error": 1, "message": "User does not exists"});
+            } else res.status(404).json({message: "User does not exists"});
         });
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json({message: 'Error occured while connecting to the database'});
     });
 });
 
